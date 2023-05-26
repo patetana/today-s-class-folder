a0f58480f82adbf556ec97b53281133d811d9250
Tanay Patel <patetana@sheridancollege.ca>
2023-05-26 09:42:08 -0400

    Addedd part time student

---
+++ src/arraydemos23friam/PartTimeStudent.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemos23friam;
+
+/**
+ *
+ * @author tanay
+ */
+public class PartTimeStudent {
+    
+}
